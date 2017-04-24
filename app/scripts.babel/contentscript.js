67042302b863424de078c4c4832de4b134f66e5f
Adrienne <adrienne@codeforamerica.org>
2017-04-23 22:28:19 -0800

    Added pop ups to username and password fields

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import {suggestion} from 'suggestion';
-
 const whitelistUrls = ['https://hiringcenter.walmartstores.com', 'https://www.facebook.com'];
 const domainRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im;
 const USERNAME = 'adriennejobs@gmail.com';
@@ -15,6 +13,13 @@ const isSupportedWebsite = function(website) {
     }, false);
 };
 
+let data = {
+    name: 'Adrienne',
+    address: 'I live here',
+    username: USERNAME,
+    password: PASSWORD
+};
+
 
 const isLoginScreen = function () {
   const passwordFields = $('input[type="password"]');
@@ -24,6 +29,42 @@ const isLoginScreen = function () {
 const isSignupScreen = function () {
   const passwordFields = $('input[type="password"]');
   return passwordFields.length === 2;
+};
+
+const insertSuggestion = function (el, id) {
+    $(`<span class="icon-resume-icon edit-suggestion" data-id="${id}"><span class="path1"></span><span class="path2"></span></span>`).insertAfter(el);
+};
+
+const insertSuggestionBox = function (dataId) {
+    $(`<div class="suggestion" data-id=${dataId}></div>`).insertAfter(`span[data-id=${dataId}]`);
+    $(`.suggestion[data-id="${dataId}"]`).load('chrome-extension://plianlcbijbdlkhojhgejpfhklhcngfc/suggestion.html', function () {
+        $(`div[data-id="${dataId}"] .fill-in-btn`).click((evt) => {
+            const key = $(`div[data-id="${dataId}"] option:selected`).val(),
+                  value = data[key];
+
+            $(`span[data-id="${dataId}"]`).prev().val(value);
+
+            return false;
+        });
+
+        $(`div[data-id="${dataId}"] .icon-cross`).click((evt) => {
+            $(`.suggestion[data-id="${dataId}"]`).remove();
+            $(`.edit-suggestion[data-id=${dataId}]`).show();
+
+        })
+    });
+
+
+};
+
+const setSuggestionListeners = function () {
+  $('.edit-suggestion').click(function (evt) {
+      const parent = $(evt.target).parent();
+      parent.hide();
+
+    insertSuggestionBox(parent.data('id'));
+
+  });
 };
 
 
@@ -50,7 +91,10 @@ $(document).ready(function() {
         usernameInput.val(USERNAME);
         passwordInput.val(PASSWORD);
 
-        suggestion();
+        insertSuggestion('form input[type="text"]', 'username');
+        insertSuggestion('form input[type="password"]', 'password');
+        setSuggestionListeners();
     }
+
 });
 
