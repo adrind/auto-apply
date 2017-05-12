740d0b47e343c4c6c3b7efbc9282cc999430f950
Adrienne <adrienne@codeforamerica.org>
2017-05-11 17:33:57 -0700

    Added login with facebook button

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -3,6 +3,7 @@
 import DataManager from './lib/data';
 const dm = new DataManager(chrome);
 
+
 const FB_CLIENT_ID = 1523477781017871,
       FB_LOGIN_REDIRECT_URI = 'http://localhost:3000/auth';
 
@@ -24,7 +25,7 @@ chrome.runtime.onMessage.addListener(({message, data}, sender, sendResponse) => 
     }
 
     if(message === 'facebook') {
-      window.open(`https://www.facebook.com/v2.9/dialog/oauth?client_id=${FB_CLIENT_ID}&redirect_uri=${FB_LOGIN_REDIRECT_URI}`);
+      window.open(`https://www.facebook.com/v2.9/dialog/oauth?client_id=${FB_CLIENT_ID}&redirect_uri=${FB_LOGIN_REDIRECT_URI}&scope=public_profile,user_education_history`);
     }
 
     //needed to make sure extension knows that sendResponse async
